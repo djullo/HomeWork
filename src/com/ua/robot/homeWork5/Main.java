d6ffe5689c1fc08097fa9758da795121332fee70
Dmytro Dotsenko <djulo92@gmail.com>
2023-01-27 19:36:14 +0200

    homeWork5

---
+++ src/com/ua/robot/homeWork5/Main.java
@@ -0,0 +1,18 @@
+package com.ua.robot.homeWork5;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 4;
+        if (number > -22) {
+            System.out.println(number + " > -22");
+        }
+        int number2 = -6;
+        if (number2 < 32) {
+            System.out.println(number2 + " < 32");
+        }
+        int number4 = 66;
+        if (number4 % 2 == 0) {
+            System.out.println(number4 + " = is even");
+        }
+    }
+}
