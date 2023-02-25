0974a484a1c9434d5a844f97273dafde66240bfb
Dmytro Dotsenko <djulo92@gmail.com>
2023-02-25 11:22:24 +0200

    homeWork14

--- src/com/ua/robot/homeWork12/Human.java
+++ src/com/ua/robot/homeWork12/Human.java
@@ -3,18 +3,18 @@ package com.ua.robot.homeWork12;
 public class Human {
 
     static {
-        System.out.println("1) Human block static");
+        System.out.println("1) Mammals block static");
     }
 
     {
-        System.out.println("3) Human block first");
+        System.out.println("3) Mammals block first");
     }
 
     {
-        System.out.println("4) Human block second");
+        System.out.println("4) Mammals block second");
     }
 
     public Human() {
-        System.out.println("5) Human constructor");
+        System.out.println("5) Mammals constructor");
     }
 }
---
+++ src/com/ua/robot/homeWork14/Main.java
@@ -0,0 +1,10 @@
+package com.ua.robot.homeWork14;
+
+public class Main {
+    public static void main(String[] args) {
+        Mammals student = new Student(30, 114);
+        Mammals teacher = new Teacher(20, 115);
+        student.print();
+        teacher.print();
+    }
+}
